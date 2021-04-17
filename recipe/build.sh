74b3fc43976757a11343efb094acb40a0af50a95
Tobias Fischer <info@tobiasfischer.info>
2021-04-18 06:07:14 +1000

    Fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS .. \
+cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
